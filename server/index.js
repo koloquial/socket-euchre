87a5f677086ef004fad15b815fb9b71b8322352c
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2022-01-17 21:39:30 -0600

    init / socket serv / basic join

---
+++ server/index.js
@@ -0,0 +1,99 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }}
+);
+
+const games = [];
+
+io.on("connection", (socket) => {
+    console.log("connected:", socket.id);
+    //emit open games to connecting client
+    socket.emit('open_games', games);
+
+    socket.on("create_game", (data) => {
+        //check if user created other games
+        //check if user active in other games
+
+        //join room
+        socket.join(data.id);
+
+        //push to games
+        const newGame = {
+            host: data.id,
+            hostName: data.name,
+            status: 'waiting',
+            players: [
+                {
+                    id: data.id, 
+                    name: data.name,
+                    hand: []
+                }
+            ],
+            turn: '',
+            score: [0, 0]
+        };
+        games.push(newGame);
+
+        //emit games to all clients
+        io.emit('open_games', games);
+    });
+
+    socket.on("get_game", (data) => {
+        for(let i = 0; i < games.length; i++){
+            if(games[i].host === data){
+                socket.emit("update_game", games[i]);
+                break;
+            }
+        }
+    })
+
+    socket.on("join_game", (data) => {
+        for(let i = 0; i < games.length; i++){
+            if(games[i].host === data.game.host){
+
+                //check player length
+                if(games[i].players.length < 4){
+                    //join room
+                    socket.join(data.game.host);
+
+                    //add player to game
+                    const newPlayer = {
+                        id: data.id, 
+                        name: data.name,
+                        hand: []
+                    }
+
+                    if(games[i].players.length === 4){
+                        games[i].status = 'active';
+                    }
+
+                    games[i].players.push(newPlayer);
+                    //send game to client
+                    socket.emit("update_game", games[i]);
+
+                    //send game to all players
+                    socket.to(data.game.host).emit("update_game", games[i])
+
+                    //send update to all clients
+                    io.emit('open_games', games);
+                }
+            }
+        }
+    });
+
+    socket.on("disconect", () => {
+        console.log('disconnected:', socket.id);
+    });
+});
+
+server.listen(3001, () => {
+    console.log('SERVER RUNNING')
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [name, setName] = useState('');
+  const [game, setGame] = useState();
+  const [openGames, setOpenGames] = useState([]);
+
+  socket.on("open_games", (data) => {
+    setOpenGames(data);
+  });
+
+  socket.on("update_game", (data) => {
+    console.log(data);
+    setGame(data);
+  })
+
+  useEffect(() => {
+    
+  });
+
+  const handleName = (e) => {
+    e.preventDefault();
+    setName(e.target.value)
+  }
+
+  const joinGame = (game) => {
+    let temp = '';
+
+    if(name === ''){
+      temp = socket.id.substring(0, 10);
+    }else if(name.length > 10){
+      temp = name.substring(0, 10);
+    }else{
+      temp = name;
+    }
+
+    socket.emit("join_game", {id: socket.id, name: temp, game: game});
+  }
+
+  const createGame = () => {
+    let temp = '';
+
+    if(name === ''){
+      temp = socket.id.substring(0, 10);
+    }else if(name.length > 10){
+      temp = name.substring(0, 10);
+    }else{
+      temp = name;
+    }
+
+    const newGame = {
+      id: socket.id,
+      name: temp
+    }
+
+    socket.emit("create_game", newGame);
+    socket.emit("get_game", socket.id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Euchre</h1>
+        </Col>
+      </Row>
+    {game ? <>
+    Waiting for players...<br /><br />
+    {game.players.map((player, index) => {
+      return (
+        <>{index + 1}. {player.name}<br /></>
+      )
+    })}
+    {console.log('game:', game)}
+    </> : <>
+    
+      <Row>
+        <Col>
+        <br />
+          <input type='text' placeholder='Enter Player Name' value={name} onChange={handleName} />
+          <p style={{fontSize: 'small'}}>ID: {socket.id}</p>
+          <button onClick={createGame}>Create Game</button><br />
+        </Col>
+        </Row>
+
+        <br /><br />
+
+        <Row>
+        <Col>
+          <h3>Join Game</h3>
+          {[...openGames].reverse().map(game => {
+            return (
+              <>
+                {game.status === 'waiting' ? 
+                  <div className='open-games' onClick={() => joinGame(game)}>
+                    <Row>
+                      <Col><b>Host:</b> {game.hostName}</Col>
+                      <Col><b>Players:</b> {game.players.length}/4</Col>
+                    </Row>
+                  </div> : <></>}
+              </>
+              
+            )
+          })}
+        </Col>
+      </Row>
+    </>}
+      
+    </Container>
   );
 }
 
